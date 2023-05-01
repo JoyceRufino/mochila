726255376e32735113935c947c335560c20d413b
joyce <joyce_rufino95@hotmail.com>
2023-05-01 13:47:07 -0300

    interagindo com dados da web

--- js/main.js
+++ js/main.js
@@ -10,37 +10,47 @@
 // no js quando temos um par de elementos, uma chave-valor, e queremos salvar o dicionario, usamos um objeto, por isso criamos const itemAtual, para transformar em objeto.
 // ao inserir esse objeto(itemAtual) no localSotrage, ele retorna um objeto e precisamos que seja uma string, então transformamos em uma string atraves de JSON.stringfy(itemAtual)
 // precismos de um array de objetos para parar de sobrescrever.
-
+// itens ja estão sendo salvos no localStorage, porém ao recarregar a página os dados ainda não ficam na tela, apenas no localStorage. preciso trazer os dados para poder manipula-los, para isso preciso fazer uma consulta no localStorage const itens = localStorage.getItem("itens") || [];
+//refatorando código, por erros de lógica. A função criaElemento agora é unica e exclusiva para criar elemento, o submit agora gerencia todas as função que precisam ser enviadas neste momento do envio do item, e página é carregada buscando dados do localStorage
 
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-const itens = [];
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+console.log(itens);
+
+itens.forEach((elemento) => {
+  criaElemento(elemento);
+});
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const nome = e.target.elements["nome"];
   const quantidade = e.target.elements["quantidade"];
-  criaElemento(nome.value, quantidade.value);
+
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  criaElemento(itemAtual);
+
+  itens.push(itemAtual);
+  localStorage.setItem("itens", JSON.stringify(itens));
+
   nome.value = "";
   quantidade.value = "";
 });
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
   const novoItem = document.createElement("li");
   novoItem.classList.add("item");
 
   const numeroItem = document.createElement("strong");
-  numeroItem.innerHTML = quantidade;
+  numeroItem.innerHTML = item.quantidade;
 
   novoItem.appendChild(numeroItem);
-  novoItem.innerHTML += nome;
+  novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
-
-  const itemAtual = {
-    nome: nome,
-    quantidade: quantidade,
-  };
-
-  itens.push(itemAtual);
-  localStorage.setItem("item", JSON.stringify(itens));
 }

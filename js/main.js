a7352150a903f4390b46e5b2c5c141d5d96dcf2e
joyce <joyce_rufino95@hotmail.com>
2023-05-01 12:43:33 -0300

    armazenando dados na web

--- js/main.js
+++ js/main.js
@@ -5,16 +5,23 @@
 // e - é o parametro que vou passar nessa função.
 // pegar os dados do formulario, capturando o input nome e quantidade
 //criar um elemento a partir do js
+// salvar informações no navegador localStorage
+// os itens estao sendo sobrescritos no localstorage, precisamos salvar multiplos dados.
+// no js quando temos um par de elementos, uma chave-valor, e queremos salvar o dicionario, usamos um objeto, por isso criamos const itemAtual, para transformar em objeto.
+// ao inserir esse objeto(itemAtual) no localSotrage, ele retorna um objeto e precisamos que seja uma string, então transformamos em uma string atraves de JSON.stringfy(itemAtual)
+// precismos de um array de objetos para parar de sobrescrever.
+
 
 const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista")
-
+const lista = document.getElementById("lista");
+const itens = [];
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  criaElemento(
-    e.target.elements["nome"].value,
-    e.target.elements["quantidade"].value
-  );
+  const nome = e.target.elements["nome"];
+  const quantidade = e.target.elements["quantidade"];
+  criaElemento(nome.value, quantidade.value);
+  nome.value = "";
+  quantidade.value = "";
 });
 
 function criaElemento(nome, quantidade) {
@@ -27,7 +34,13 @@ function criaElemento(nome, quantidade) {
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += nome;
 
-  lista.appendChild(novoItem)
+  lista.appendChild(novoItem);
 
+  const itemAtual = {
+    nome: nome,
+    quantidade: quantidade,
+  };
+
+  itens.push(itemAtual);
+  localStorage.setItem("item", JSON.stringify(itens));
 }
-
